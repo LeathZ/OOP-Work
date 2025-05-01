8d278d4ad5bbfb016e38976686d2a497747701a2
Lajesha Sambahamphe <123lajesha@gmail.com>
2025-05-01 08:28:41 +0545

    Second task

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,13 +1,2 @@
-public class Task{
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//         System.out.print("Enter number:");
-//         int a=sc.nextInt();
-//         int b=0;
-//         for(int i=1;i<=a;i++){
-//             b=b+i;
-//         }
-//         System.out.print(b);
-//         sc.close();
-//     }
-// }
+System.out.print("Enter:");
+    int a=sc.nextInt();
